1ec7606bdfd73760547a6505ef2c36dd93509f4a
raingang <raingang4@gmail.com>
2017-08-29 15:04:22 +0300

    Added admin panel to Products and Category

--- zoo_store/store/admin.py
+++ zoo_store/store/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
-from .models import Product, Category 
+from .models import Product, Category
 
-admin.site.register(Product)
-admin.site.register(Category)
+
+class CategoryAdmin(admin.ModelAdmin):
+    '''
+    Модель отображения категории в админке
+    '''
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name', )}
+
+
+class ProductAdmin(admin.ModelAdmin):
+    '''
+    Модель отображения товара в админке
+    '''
+    list_display = ['name', 'slug', 'price', 'copies',
+                    'discount', 'created', 'updated', 'category']
+    list_filter = ['category', 'created', 'updated']
+    list_editable = ['price', 'copies', 'discount']
+    prepopulated_fields = {'slug': ('name', )}
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
 # Register your models here.
